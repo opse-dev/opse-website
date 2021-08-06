a0eb00548e1584d3bcbd192649f6db2763b5c796
Taimoor-Tariq <taimoorhero15@gmail.com>
2021-08-06 15:09:36 -0400

    Update page title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/global.scss';
+import "../styles/global.scss";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Ontario Post-Secondary Esports | The Future of School Spirit</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,6 @@
-import Head from 'next/head';
-
 function HomePage() {
     return (
-        <>
-            <Head>
-            <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        
+        <>        
             <h1>Hello World!</h1>
         </>
     )
